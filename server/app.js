456251af9dfe5e1da553e10d409b940f91305802
Matan Greenvlad <mgk2798@gmail.com>
2020-10-04 14:01:31 +0300

    solution

--- server/app.js
+++ server/app.js
@@ -5,6 +5,17 @@ const app = express();
 app.use(express.json());
 
 /* Enter code Below */
+const messages = [];
+
+app.get('/messages', (req, res) => {
+    res.send(messages);
+})
+
+app.post('/messages', (req, res) => {
+    const newMessage = req.body
+    messages.push(newMessage)
+    res.send('sent');
+})
 
 
 
